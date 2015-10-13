e02aedef4c5417e365174670cd43dccfd503d467
Terrance Woest <terrance@tremli.com>
2015-10-13 12:57:21 -0700

    Made it so parent theme styles is a dependency of child theme styles.

--- functions/scripts.php
+++ functions/scripts.php
@@ -5,5 +5,7 @@ add_action('wp_enqueue_scripts', 'bring_in_parent_styles');
 
 function bring_in_parent_styles() {
 	// Bring's in Genesis's Default styling first so child theme overwrites it.
-	wp_enqueue_style('parent_theme_styles', get_template_directory_uri() . '/style.css', 5);
+	wp_register_style('parent_theme_styles', get_template_directory_uri() . '/style.css', 1);
+	wp_register_style('child_theme_styles', get_stylesheet_uri(), array('parent_theme_styles'));
+	wp_enqueue_style('child_theme_styles');
 }
